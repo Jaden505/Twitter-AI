c6be23000250b471f931c1b1bc2dccdfaa1e31ea
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-02-18 22:45:12 +0100

    Added env variable

--- data.py
+++ data.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import sqlalchemy as db
-import os
+from decouple import config
+
 
 df = pd.read_csv('trainingandtestdata/training.1600000.processed.noemoticon.csv', sep=',', encoding='ISO-8859-1')
 df.head()
 
 # create db first in MySQL
 ssl = {"ssl": {"ssl_ca": "ca.pem", "ssl_cert": "client-cert.pem","ssl_key": "client-key.pem"}}
-password = os.environ.get('DATABASE_PASSWORD')
+password = config('MYSQL_OEGE_DATABASE_PASSWORD')
 print(password)
 engine = db.create_engine(f'mysql+pymysql://rijswij2:{password}@oege.ie.hva.nl/zrijswij2', connect_args=ssl)
 
