4cdfce675e4df85783d17bd8f898fc1aea86f522
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-02-19 15:02:32 +0100

    Saved models and swapped svm for random forest

--- models.py
+++ models.py
@@ -1,46 +1,42 @@
 from data import Data
 
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.svm import SVC
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 class Models:
-    def __init__(self):
-        pass
-
     def naive_bayes(self):
         # Convert the text data into a matrix of token counts
         vectorizer = CountVectorizer()
         X_train_vec = vectorizer.fit_transform(X_train)
 
-        # Train a Naive Bayes classifier on the training data
         clf = MultinomialNB()
         clf.fit(X_train_vec, y_train)
 
-        # Convert the test data into a matrix of token counts
         X_test_vec = vectorizer.transform(X_test)
 
         # Evaluate the accuracy of the classifier on the test data
         accuracy = clf.score(X_test_vec, y_test)
         print("Accuracy:", accuracy)
 
-    def svm(self):
-        # Convert the text data into a matrix of TF-IDF features
-        vectorizer = TfidfVectorizer()
+        pickle.dump(clf, open('nb_model.sav', 'wb'))
+
+    def random_forest(self):
+        # Convert the text data into a matrix of word counts
+        vectorizer = CountVectorizer()
         X_train_vec = vectorizer.fit_transform(X_train)
 
-        # Train an SVM classifier on the training data
-        clf = SVC(kernel='linear')
+        clf = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
         clf.fit(X_train_vec, y_train)
 
-        # Convert the test data into a matrix of TF-IDF features
         X_test_vec = vectorizer.transform(X_test)
 
         # Evaluate the accuracy of the classifier on the test data
         accuracy = clf.score(X_test_vec, y_test)
         print("Accuracy:", accuracy)
 
+        pickle.dump(clf, open('rf_model.sav', 'wb'))
 
 if __name__ == '__main__':
     d = Data()
@@ -48,5 +44,5 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = d.shape_data()
 
     m = Models()
-    m.svm()
-    m.naive_bayes()
+    # m.naive_bayes()
+    m.random_forest()
