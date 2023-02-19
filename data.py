86a65489737b7941de27d82f33691025dfe6aee2
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-02-19 13:19:25 +0100

    Date correctly shaped

--- data.py
+++ data.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import sqlalchemy as db
 from decouple import config
+from sklearn.model_selection import train_test_split
+import re
 
 class Data:
     def __init__(self):
-        pass
+        self.df = None
 
-    def create_database(self):
-        self.get_local_data().to_sql(name='tweet', con=engine, if_exists='fail', index=False,
+    def create_database(self, engine):
+        self.df.to_sql(name='tweet', con=engine, if_exists='fail', index=False,
                                      dtype={
                                          'polarity': db.types.INTEGER(),
                                          'id': db.types.BIGINT(),
@@ -17,30 +19,35 @@ class Data:
                                      })
 
     def get_local_data(self):
-        df = pd.read_csv('trainingandtestdata/training.1600000.processed.noemoticon.csv', sep=',',
+        self.df = pd.read_csv('trainingandtestdata/training.1600000.processed.noemoticon.csv', sep=',',
                          encoding='ISO-8859-1')
-        df.columns = ['polarity', 'id', 'date', 'query', 'user', 'text']
-        return df
+        self.df.columns = ['polarity', 'id', 'date', 'query', 'user', 'text']
 
     def get_database_data(self):
         ssl = {"ssl": {"ssl_ca": "ca.pem", "ssl_cert": "client-cert.pem", "ssl_key": "client-key.pem"}}
         password = config('MYSQL_OEGE_DATABASE_PASSWORD')
         engine = db.create_engine(f'mysql+pymysql://rijswij2:{password}@oege.ie.hva.nl/zrijswij2', connect_args=ssl)
         conn = engine.connect()
-        metadata = db.MetaData()
 
-        tweets = db.Table('tweet', metadata, autoload_with=engine)
-
-        df = pd.read_sql_table('tweet', con=conn)
-        df.columns = ['polarity', 'id', 'date', 'query', 'user', 'text']
+        self.df = pd.read_sql_table('tweet', con=conn)
+        self.df.columns = ['polarity', 'id', 'date', 'query', 'user', 'text']
 
         conn.close()
-        print(df.head(2))
 
-        return df
+    def shape_data(self):
+        x = self.df.loc[:, ['date', 'text']]
+        x['date'] = x['date'].apply(lambda x: re.search(r'\b\d{2}(?=:)', x).group(0))
+        x['date'] = x['date'].astype('int').values
+
+        y = self.df['polarity']
+        y = y.astype('int').values
+
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+        return X_train, X_test, y_train, y_test
 
 
 if __name__ == '__main__':
     d = Data()
-    d.get_database_data()
-
+    d.get_local_data()
+    X_train, X_test, y_train, y_test = d.shape_data()
