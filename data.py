a37de2177f6c4218e944c2ac8709ffc7643833c5
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-02-19 11:57:19 +0100

    Succesfully gets data from database and conferts to dataframe

--- data.py
+++ data.py
@@ -2,18 +2,45 @@ import pandas as pd
 import sqlalchemy as db
 from decouple import config
 
+class Data:
+    def __init__(self):
+        pass
 
-df = pd.read_csv('trainingandtestdata/training.1600000.processed.noemoticon.csv', sep=',', encoding='ISO-8859-1')
-df.head()
+    def create_database(self):
+        self.get_local_data().to_sql(name='tweet', con=engine, if_exists='fail', index=False,
+                                     dtype={
+                                         'polarity': db.types.INTEGER(),
+                                         'id': db.types.BIGINT(),
+                                         'date': db.types.VARCHAR(255),
+                                         'query': db.types.VARCHAR(255),
+                                         'user': db.types.VARCHAR(900),
+                                     })
 
-# create db first in MySQL
-ssl = {"ssl": {"ssl_ca": "ca.pem", "ssl_cert": "client-cert.pem","ssl_key": "client-key.pem"}}
-password = config('MYSQL_OEGE_DATABASE_PASSWORD')
-print(password)
-engine = db.create_engine(f'mysql+pymysql://rijswij2:{password}@oege.ie.hva.nl/zrijswij2', connect_args=ssl)
+    def get_local_data(self):
+        df = pd.read_csv('trainingandtestdata/training.1600000.processed.noemoticon.csv', sep=',',
+                         encoding='ISO-8859-1')
+        df.columns = ['polarity', 'id', 'date', 'query', 'user', 'text']
+        return df
 
-df.to_sql(name='tweet', con=engine, if_exists='fail', index=False)
-conn = engine.connect()
+    def get_database_data(self):
+        ssl = {"ssl": {"ssl_ca": "ca.pem", "ssl_cert": "client-cert.pem", "ssl_key": "client-key.pem"}}
+        password = config('MYSQL_OEGE_DATABASE_PASSWORD')
+        engine = db.create_engine(f'mysql+pymysql://rijswij2:{password}@oege.ie.hva.nl/zrijswij2', connect_args=ssl)
+        conn = engine.connect()
+        metadata = db.MetaData()
 
-metadata = db.MetaData()
-tweets = db.Table('tweet', metadata, autoload_with=engine)
+        tweets = db.Table('tweet', metadata, autoload_with=engine)
+
+        df = pd.read_sql_table('tweet', con=conn)
+        df.columns = ['polarity', 'id', 'date', 'query', 'user', 'text']
+
+        conn.close()
+        print(df.head(2))
+
+        return df
+
+
+if __name__ == '__main__':
+    d = Data()
+    d.get_database_data()
+
